a4b63b04485bd52d13f461586d48e464df4e418c
tero1115 <limjisang1115@gmail.com>
2023-03-30 10:40:20 +0900

    메인 화면 만들기

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_kakao/screens/chat_screen.dart';
+import 'package:flutter_kakao/screens/friend_screen.dart';
+import 'package:flutter_kakao/screens/more_screen.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -8,11 +12,38 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+
+  // 상태 변수
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("MainScreen"),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: [
+          FrientScreen(),
+          ChatScreen(),
+          MoreScreen(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        backgroundColor: Colors.grey[100],
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black54,
+        onTap: (index){
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        currentIndex: _selectedIndex,
+        items: [
+          BottomNavigationBarItem(icon: Icon(FontAwesomeIcons.user), label: ""),
+          BottomNavigationBarItem(icon: Icon(FontAwesomeIcons.comment), label: ""),
+          BottomNavigationBarItem(icon: Icon(FontAwesomeIcons.ellipsisH), label: ""),
+        ],
       ),
     );
   }
